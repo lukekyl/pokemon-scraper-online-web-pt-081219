555dca23738259ded6e7da90ea0e7303924cc86b
Kyle Luke <th.oldschool@gmail.com>
2019-10-28 02:29:10 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
   end
 
   def find(number, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE name = ?",[number])
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?",[number])
     result = DB[:conn].execute(sql, name)[0]
     Pokemon.new(result[0], result[1], result[2], result[3])
   end
